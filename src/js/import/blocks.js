6f9709219da3142d95db15a501d6387d087b8f60
Angelina Yermakova <ermakova.lina.lina@gmail.com>
2019-06-11 05:57:38 +0300

    add login/pass, business, price

--- src/js/import/blocks.js
+++ src/js/import/blocks.js
@@ -1,1 +1,2 @@
 import '../../views/blocks/user-panel/user-panel'
+import '../../views/blocks/business-panel/business-panel'
--- src/views/blocks/user-panel/user-panel.js
+++ src/views/blocks/user-panel/user-panel.js
@@ -6,23 +6,3 @@ $(".show-password").on('click',function() {
         $input.attr('type', 'password');
     }
 });
-
-// var clogin = $("#invest-login-tab");
-// var cregister = $("#invest-registr-tab");
-// /* display the register page */
-// $("#invest-registr-nav").on("click", function(e){
-//     e.preventDefault();
-//     var newheight = cregister.height();
-//     $("#invest-tabContent").stop().animate({
-//         "height": newheight+"px"
-//     }, 550);
-// });
-//
-// /* display the login page */
-// $("#invest-login-nav").on("click", function(e){
-//     e.preventDefault();
-//     var newheight = clogin.height();
-//     $("#invest-tabContent").stop().animate({
-//         "height": newheight+"px"
-//     }, 550);
-// });
